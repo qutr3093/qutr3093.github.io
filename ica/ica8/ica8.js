1735162707b83833bf1cfd4798730993fcfe6202
qutr3093 <qutr3093@colorado.edu>
2022-10-11 11:00:15 -0600

    js

---
+++ ica/ica8/ica8.js
@@ -0,0 +1,15 @@
+let body = document.querySelector("div");
+body.addEventListener("click", giveAlert);
+
+function giveAlert(){
+    alert("HEY SHAWTYYYYYY")
+}
+
+var button = document.querySelector("button");
+var box = document.getElementsByClassName("box");
+
+button.addEventListener('click', function onClick() {
+    button.style.backgroundColor = 'salmon';
+  });
+
+
