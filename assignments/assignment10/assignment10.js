55e4230f51d99e9d092d19ec54275190ba427b6b
qutr3093 <qutr3093@colorado.edu>
2022-10-30 18:13:51 -0600

    json updates

--- assignments/assignment10/assignment10.js
+++ assignments/assignment10/assignment10.js
@@ -1,102 +1,106 @@
 // Q1
 var resources = {
-        "employees" : [
-            {
-                "name" : "Sam",
-                "department" : "Tech",
-                "designation" : "Manager",
-                "salary" : 40000,
-                "raise" : true,
-            },
-            {
-                "name" : "Mary",
-                "department" : "Finance",
-                "designation" : "Trainee",
-                "salary" : 18500,
-                "raise?" : true,
-            },
-            {
-                "name" : "Bill",
-                "department" : "HR",
-                "designation" : "Executive",
-                "salary" : 21200,
-                "raise" : false,
-            }
-        ]
-    }
-    
-    console.log (resources.employees)
-    console.log (resources.employees[2].name)
-    
-    // Q2
-    var resources = {
-        "companyName" : "Tech Stars",
-        "website" : "www.techstars.site",
-        "employees" : [
-            {
-                "name" : "Sam",
-                "department" : "Tech",
-                "designation" : "Manager",
-                "salary" : 40000,
-                "raise" : true,
-            },
-            {
-                "name" : "Mary",
-                "department" : "Finance",
-                "designation" : "Trainee",
-                "salary" : 18500,
-                "raise" : true,
-            },
-            {
-                "name" : "Bill",
-                "department" : "HR",
-                "designation" : "Executive",
-                "salary" : 21200,
-                "raise" : false,
-            },
-        ]
-    }
-    
-    console.log(resources.companyName)
-    console.log(resources.website)
-    
-    // Q3
-    
-    let newEmployee = 
-        {
-            "name" : "Anna",
-            "department" : "Tech",
-            "designation" : "Executive",
-            "salary" : 25600,
-            "raise?" : false,
-        }
-    
-    resources["employees"].push (newEmployee)
-    
-    console.log(resources.employees.length)
-    console.log(resources.employees[3].name)
-    
-    // Q4 
-    
-    let total = 0
-    for (let i = 0; i < resources.employees.length; i++) { //this iterates through employee salaries, how can you use this to find the total salary for #4?
-        // console.log(resources["employees"][i].salary); 
-        // console.log()
-        total += resources.employees[i].salary
-      }
-    
-    console.log(total)
-
-    // Q5
-    let newSalary = 0;
-    function raiseSalary(){
-        for (let j = 0; j < resources.employees.raise.length; j++){
-        if (resources.employees.raise?[j] === "true" ){
-            resources.employees.raise *= 0.1
+    "employees" : [
+        {
+            "name" : "Sam",
+            "department" : "Tech",
+            "designation" : "Manager",
+            "salary" : 40000,
+            "raise" : true,
+        },
+        {
+            "name" : "Mary",
+            "department" : "Finance",
+            "designation" : "Trainee",
+            "salary" : 18500,
+            "raise" : true,
+        },
+        {
+            "name" : "Bill",
+            "department" : "HR",
+            "designation" : "Executive",
+            "salary" : 21200,
+            "raise" : false,
         }
-        else{
+    ]
+}
 
-        }
+console.log (resources.employees)
 
+// Q2
+var company = {
+    "companyName" : "Tech Stars",
+    "website" : "www.techstars.site",
+    "employees" : [
+        {
+            "name" : "Sam",
+            "department" : "Tech",
+            "designation" : "Manager",
+            "salary" : 40000,
+            "raise" : true,
+        },
+        {
+            "name" : "Mary",
+            "department" : "Finance",
+            "designation" : "Trainee",
+            "salary" : 18500,
+            "raise" : true,
+        },
+        {
+            "name" : "Bill",
+            "department" : "HR",
+            "designation" : "Executive",
+            "salary" : 21200,
+            "raise" : false,
+        },
+    ]
+}
+
+console.log(company)
+
+// Q3
+let newEmployee = 
+    {
+        "name" : "Anna",
+        "department" : "Tech",
+        "designation" : "Executive",
+        "salary" : 25600,
+        "raise" : false,
     }
 
+company["employees"].push(newEmployee)
+
+console.log(company.employees.length)
+console.log(company.employees)
+console.log(company.employees[3])
+
+// Q4
+let total = 0
+for (let i = 0; i < company.employees.length; i++) {
+    total += company.employees[i].salary
+}
+console.log(total)
+
+// Q5
+for (let j = 0; j < company.employees.length; j++) {
+    if (company.employees[j].raise === true) {
+        let increaseSalary = company.employees[j].salary * 1.1
+        company.employees[j].salary = increaseSalary
+        company.employees[j].raise = false
+    }  
+}
+
+console.log(company.employees)
+
+// Q6
+for (let k = 0; k < company.employees.length; k++) {
+    if (company.employees[k].name === "Anna") {
+        company.employees[k].wfh = true
+    } else if (company.employees[k].name === "Sam") {
+        company.employees[k].wfh = true
+    } else {
+        company.employees[k].wfh = false
+    }
+}
+
+console.log(company.employees)
