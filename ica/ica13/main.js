c1d3551f090e72660642f07440f14136663f03a6
qutr3093 <qutr3093@colorado.edu>
2022-11-17 14:22:25 -0700

    Update main.js

--- ica/ica13/main.js
+++ ica/ica13/main.js
@@ -12,3 +12,40 @@ const aliceTumbling = [
   const alice1 = document.querySelector("#alice1");
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
+
+  // alice1.animate(aliceTumbling, aliceTiming);
+  // alice2.animate(aliceTumbling, aliceTiming);
+  // alice3.animate(aliceTumbling, aliceTiming);
+
+  //--------------------------------------------------------------
+  //callback
+  // function animation() {
+  //   alice1.animate(aliceTumbling, aliceTiming).finished.then(()=>{
+  //     alice2.animate(aliceTumbling, aliceTiming).finished.then(()=>{
+  //       alice3.animate(aliceTumbling, aliceTiming);
+  //     })
+  //   }) 
+  // } 
+  // animation();
+
+//--------------------------------------------------------------
+  // promise chain , arrow function
+  const animation = alice1.animate(aliceTumbling, aliceTiming)
+
+  animation.finished
+  .then(()=> alice2.animate(aliceTumbling, aliceTiming).finished)
+  .then(()=> alice3.animate(aliceTumbling, aliceTiming))
+
+//--------------------------------------------------------------
+  //async and await
+  // async function animation() {
+  //   try {
+  //     await alice1.animate(aliceTumbling, aliceTiming).finished;
+  //     await alice2.animate(aliceTumbling, aliceTiming).finished;
+  //     alice3.animate(aliceTumbling, aliceTiming)
+  //   }
+  //   catch(error){
+  //     console.error(`Could not play the animation: ${error}`)
+  //   }
+  // } 
+  // animation();
